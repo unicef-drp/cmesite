6ffad44c47bf254812ce8b5307dd5b1ee3c9d717
eric-basley <eric.basley@redpelicans.com>
2018-06-20 23:02:51 +0200

    On the road to CD...

--- src/web/index.js
+++ src/web/index.js
@@ -1,4 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<div>Hello world</div>, document.getElementById('__root__'));
+ReactDOM.render(<div>Hello UNICEF</div>, document.getElementById('__root__'));
