10234b95e1f77d9bc769454044dfc0e7c8ebc35e
Nicolas Briemant <nicolas-briemant@users.noreply.github.com>
2018-07-23 20:54:50 +0200

    add posts

--- src/web/pages/LandingPage/messages.js
+++ src/web/pages/LandingPage/messages.js
@@ -10,4 +10,8 @@ export default defineMessages({
     defaultMessage:
       'CME Info is a web portal containing all available data and the latest child mortality estimates for each country based on the research of the UN Inter-agency Group for Child Mortality Estimation.',
   },
+  postReadMore: {
+    id: 'post.readmore',
+    defaultMessage: 'Read More',
+  },
 });
