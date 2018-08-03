bd88f9b9851dafc02c00f202778abee8c73c1b43
Nicolas Briemant <nicolas-briemant@users.noreply.github.com>
2018-08-03 14:34:39 +0200

    add theme support and unicef draft theme

--- src/web/index.js
+++ src/web/index.js
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom';
 import { createBrowserHistory } from 'history';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import LanguageProvider from './components/LanguageProvider';
 import ConfigContext from './components/ConfigContext';
 import App from './components/App';
@@ -12,6 +14,24 @@ import configureStore from './store/configureStore';
 import ducks from './ducks';
 import wpApi from './api/wp';
 import loadConfig from './config';
+
+// https://material.io/tools/color/#!/?view.left=0&view.right=1&primary.color=0096FF&secondary.color=ffffff&primary.text.color=ffffff&secondary.text.color=0096ff
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#69c6ff',
+      main: '#0096ff',
+      dark: '#0069cb',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#fff',
+      main: '#fff',
+      dark: '#ccc',
+      contrastText: '#0069cb',
+    },
+  },
+});
 
 loadConfig().then(config => {
   const {
@@ -24,7 +44,10 @@ loadConfig().then(config => {
       <Provider store={store}>
         <LanguageProvider messages={translationMessages}>
           <Router history={history}>
-            <App store={store} />
+            <MuiThemeProvider theme={theme}>
+              <CssBaseline />
+              <App store={store} />
+            </MuiThemeProvider>
           </Router>
         </LanguageProvider>
       </Provider>
