1127d5ac1aacf9b0883cf2b64b8519e94f14f823
Nicolas Briemant <nicolas-briemant@users.noreply.github.com>
2018-08-10 12:47:55 +0200

    update mocks

--- src/mock/data/index.js
+++ src/mock/data/index.js
@@ -1,7 +1,64 @@
+const splash = {
+  id: 1,
+  title: { rendered: 'title of the splash' },
+  content: { rendered: 'content of the splash' },
+  acf: {
+    image: {
+      alt: 'alternative text for accessibility',
+      url: 'url of the image',
+    },
+  },
+};
+
+const news = {
+  id: 1,
+  title: { rendered: 'title of the news' },
+  content: { rendered: 'content of the news' },
+  acf: {
+    image: {
+      alt: 'alternative text for accessibility',
+      url: 'url of the image',
+    },
+  },
+};
+
+const report = {
+  id: 1,
+  title: { rendered: 'title of the report' },
+  acf: {
+    file: {
+      id: 1,
+      description: 'lang of the report (related to the file)',
+      url: 'url of the file',
+    },
+    image: {
+      alt: 'alternative text for accessibility',
+      url: 'url of the image',
+    },
+  },
+};
+
+const dataset = {
+  id: 1,
+  title: { rendered: 'title of the dataset' },
+  modified_gmt: 'updated at',
+  acf: {
+    file: {
+      id: 1,
+      description: 'subtitle of the dataset (related to the file)',
+      url: 'url of the file',
+    },
+  },
+};
+
 const data1 = { type: 'data1' };
 const data2 = { type: 'data2' };
 
 module.exports = {
   data1,
   data2,
+  news,
+  report,
+  dataset,
+  splash,
 };
