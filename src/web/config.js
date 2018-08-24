955228b9357682273b211ff846cf02e14b1be862
eric-basley <eric.basley@redpelicans.com>
2018-08-24 18:44:30 +0200

    On the road to CD...

--- src/web/config.js
+++ src/web/config.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { prop } from 'ramda';
 
-const CONFIG_PATH = '/config.json';
+const CONFIG_PATH = '/params/config.json';
 const loadConfig = () => axios.get(CONFIG_PATH).then(prop('data'));
 export default loadConfig;
