cf03176682938583f25c10de5c71cf3948aea425
Nicolas Briemant <nicolas-briemant@users.noreply.github.com>
2018-09-06 16:59:08 +0200

    add data download bar

---
+++ src/web/components/DataDownloadBar/messages.js
@@ -0,0 +1,20 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  'xml.all': {
+    id: 'download.xml.all',
+    defaultMessage: 'Download all as XML',
+  },
+  'xml.selection': {
+    id: 'download.xml.selection',
+    defaultMessage: 'Download selection as XML',
+  },
+  'csv.all': {
+    id: 'download.csv.all',
+    defaultMessage: 'Download all as CSV',
+  },
+  'csv.selection': {
+    id: 'download.csv.selection',
+    defaultMessage: 'Download selection as CSV',
+  },
+});
