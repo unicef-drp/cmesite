54f2f33d749c126b0bdf378496ca250e0fd3b75d
Nicolas Briemant <nicolas.briemant@gmail.com>
2018-10-12 15:34:14 +0200

    wip download actions impact

--- src/web/components/DataDownload/component.js
+++ src/web/components/DataDownload/component.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
-import DataDownloadBar from '../DataDownloadBar';
+import { DataDownloadBar } from '../DataDownloadActions';
 import DataDownloadDimensions from '../DataDownloadDimensions';
 
 const style = theme => ({
