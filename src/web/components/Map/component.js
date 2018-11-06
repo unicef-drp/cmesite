2249be530d61a0312cf7176d948541e52d3622a2
Nicolas Briemant <nicolas.briemant@gmail.com>
2018-11-06 17:50:07 +0100

    map

--- src/web/components/Map/component.js
+++ src/web/components/Map/component.js
@@ -3,38 +3,23 @@
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import { compose } from 'ramda';
-import { geoMercator, geoPath } from 'd3-geo';
+import { geoPath } from 'd3-geo';
+import { geoRobinson } from 'd3-geo-projection';
 import { feature } from 'topojson-client';
 import { withSize } from 'react-sizeme';
+import worldData from '../../../mock/map/world-geo';
 
 const style = () => ({});
 
 class WorldMap extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      worldData: [],
-    };
-  }
-
   projection({ width, height }) {
-    return geoMercator()
+    /*return geoMercator()
       .scale(width / 2 / Math.PI)
-      .translate([width / 2, height / 1.4]);
-  }
+      .translate([width / 2, height / 1.4]);*/
 
-  componentDidMount() {
-    fetch('https://unpkg.com/world-atlas@1.1.4/world/110m.json').then(response => {
-      if (response.status !== 200) {
-        // console.log(`There was a problem: ${response.status}`);
-        return;
-      }
-      response.json().then(worldData => {
-        this.setState({
-          worldData: feature(worldData, worldData.objects.countries).features,
-        });
-      });
-    });
+    return geoRobinson()
+      .scale(width / 2 / Math.PI)
+      .translate([width / 2, height / 2]);
   }
 
   render() {
@@ -46,14 +31,14 @@ class WorldMap extends React.Component {
         {/* div is required for withSize to work properly */}
         <svg width={width} height={height} viewBox={`0 0 ${width} ${height}`}>
           <g className="countries">
-            {this.state.worldData.map((d, i) => (
+            {worldData.features.map((d, i) => (
               <path
                 key={`path-${i}`}
                 d={geoPath().projection(this.projection({ width, height }))(d)}
-                className="country"
-                fill={`rgba(38,50,56,${1 / this.state.worldData.length * i})`}
+                fill={`rgba(38,50,56,${1 / worldData.features.length * i})`}
                 stroke="#FFFFFF"
                 strokeWidth={0.5}
+                onMouseOver={() => console.log(d)}
               />
             ))}
           </g>
