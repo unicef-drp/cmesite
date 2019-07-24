73a015125bdaa0294569cfea53c21c537fb19ed3
Nicolas Briemant <nicolas.briemant@gmail.com>
2019-07-24 17:10:44 +0200

    test saga pattern: once

--- app/containers/HomePage/saga.js
+++ app/containers/HomePage/saga.js
@@ -2,7 +2,7 @@
  * Gets the repositories of the user from Github
  */
 
-import { call, put, select, takeLatest } from 'redux-saga/effects';
+import { call, put, select, take, fork } from 'redux-saga/effects';
 import { LOAD_REPOS } from 'containers/App/constants';
 import { reposLoaded, repoLoadingError } from 'containers/App/actions';
 
@@ -34,5 +34,9 @@ export default function* githubData() {
   // By using `takeLatest` only the result of the latest API call is applied.
   // It returns task descriptor (just like fork) so we can continue execution
   // It will be cancelled automatically on component unmount
-  yield takeLatest(LOAD_REPOS, getRepos);
+  console.log(1, 'githubData')
+  yield take(LOAD_REPOS);
+  console.log(2, 'githubData')
+  yield fork(getRepos);
+  console.log(3, 'githubData')
 }
