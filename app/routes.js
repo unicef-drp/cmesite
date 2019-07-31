8bdc63ae00af93d018fdaf847c2d77375d5a8220
Nicolas Briemant <nicolas.briemant@gmail.com>
2019-07-31 16:10:19 +0200

    cyclic six

--- app/routes.js
+++ app/routes.js
@@ -1,9 +1,11 @@
 import * as R from 'ramda';
 import ReportsPage from 'containers/ReportsPage/Loadable';
+import MethodsPage from 'containers/MethodsPage/Loadable';
 import AboutPage from 'containers/AboutPage/Loadable';
 
 const routes = {
   reports: { path: '/reports', exact: true, component: ReportsPage },
+  methods: { path: '/methods', exact: true, component: MethodsPage },
   about: { path: '/about', exact: true, component: AboutPage },
 };
 
