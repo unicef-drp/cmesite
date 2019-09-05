a6b582ccd443000c91681bd4b2a13f8b253e1013
7.Mandrill <florian.chanal@redpelicans.com>
2019-09-05 14:52:09 +0200

    Complete the data reducer unit tests

--- src/web/ducks/tests/data.reducer.test.js
+++ src/web/ducks/tests/data.reducer.test.js
@@ -20,7 +20,7 @@ describe('web/ducks/data', () => {
       foo: 'bar',
       test: { number: '42' },
     };
-    describe('when called with an empty action', () => {
+    describe('when called with a empty action', () => {
       it('should return the initial state', () => {
         const action = {};
         const expectedState = initialState;
@@ -28,45 +28,49 @@ describe('web/ducks/data', () => {
       });
     });
 
-    it('should handle HIGHLIGHT_SERIE', () => {
-      const serieType = 'serie-type';
-      const serieId = 'serie-id';
-      const action = { type: HIGHLIGHT_SERIE, serieType, serieId };
-      const state = {
-        ...initialState,
-        [`${serieType}Series`]: { [serieId]: { isHighlighted: true } },
-      };
-      const expectedState = {
-        ...initialState,
-        [`${serieType}Series`]: { [serieId]: { isHighlighted: false } },
-      };
-      expect(reducer(state, action)).toEqual(expectedState);
+    describe(' HIGHLIGHT_SERIE', () => {
+      it('should match', () => {
+        const serieType = 'serie-type';
+        const serieId = 'serie-id';
+        const action = { type: HIGHLIGHT_SERIE, serieType, serieId };
+        const state = {
+          ...initialState,
+          [`${serieType}Series`]: { [serieId]: { isHighlighted: true } },
+        };
+        const expectedState = {
+          ...initialState,
+          [`${serieType}Series`]: { [serieId]: { isHighlighted: false } },
+        };
+        expect(reducer(state, action)).toEqual(expectedState);
+      });
+
+      it('should match', () => {
+        const serieType = 'serie-type';
+        const serieId = 'serie-id';
+        const action = { type: HIGHLIGHT_SERIE, serieType, serieId };
+        const expectedState = {
+          ...initialState,
+          [`${serieType}Series`]: { [serieId]: { isHighlighted: true } },
+        };
+        expect(reducer(initialState, action)).toEqual(expectedState);
+      });
     });
 
-    it('should handle HIGHLIGHT_SERIE', () => {
-      const serieType = 'serie-type';
-      const serieId = 'serie-id';
-      const action = { type: HIGHLIGHT_SERIE, serieType, serieId };
-      const expectedState = {
-        ...initialState,
-        [`${serieType}Series`]: { [serieId]: { isHighlighted: true } },
-      };
-      expect(reducer(initialState, action)).toEqual(expectedState);
-    });
+    describe('HIGHLIGHT_METHOD', () => {
+      it('should match', () => {
+        const methodId = 'method-id';
+        const action = { type: HIGHLIGHT_METHOD, methodId };
+        const state = { ...initialState, highlightedMethods: { [methodId]: true } };
+        const expectedState = { ...initialState, highlightedMethods: { [methodId]: false } };
+        expect(reducer(state, action)).toEqual(expectedState);
+      });
 
-    it('should handle HIGHLIGHT_METHOD', () => {
-      const methodId = 'method-id';
-      const action = { type: HIGHLIGHT_METHOD, methodId };
-      const state = { ...initialState, highlightedMethods: { [methodId]: true } };
-      const expectedState = { ...initialState, highlightedMethods: { [methodId]: false } };
-      expect(reducer(state, action)).toEqual(expectedState);
-    });
-
-    it('should handle HIGHLIGHT_METHOD', () => {
-      const methodId = 'method-id';
-      const action = { type: HIGHLIGHT_METHOD, methodId };
-      const expectedState = { ...initialState, highlightedMethods: { [methodId]: true } };
-      expect(reducer(initialState, action)).toEqual(expectedState);
+      it('should match', () => {
+        const methodId = 'method-id';
+        const action = { type: HIGHLIGHT_METHOD, methodId };
+        const expectedState = { ...initialState, highlightedMethods: { [methodId]: true } };
+        expect(reducer(initialState, action)).toEqual(expectedState);
+      });
     });
 
     it('should handle CHANGE_ACTIVE_TAB', () => {
@@ -83,33 +87,35 @@ describe('web/ducks/data', () => {
       expect(reducer(initialState, action)).toEqual(expectedState);
     });
 
-    it('should handle TOGGLE_DIMENSION_VALUE', () => {
-      const dimensionIndex = 'dimension-index';
-      const valueIndex = 'value-index';
-      const action = { type: TOGGLE_DIMENSION_VALUE, dimensionIndex, valueIndex };
-      const state = {
-        ...initialState,
-        dimensions: { [dimensionIndex]: { values: { [valueIndex]: { isToggled: true } } } },
-        compareStale: true,
-      };
-      const expectedState = {
-        ...initialState,
-        dimensions: { [dimensionIndex]: { values: { [valueIndex]: { isToggled: false } } } },
-        compareStale: true,
-      };
-      expect(reducer(state, action)).toEqual(expectedState);
-    });
+    describe('TOGGLE_DIMENSION_VALUE', () => {
+      it('should match', () => {
+        const dimensionIndex = 'dimension-index';
+        const valueIndex = 'value-index';
+        const action = { type: TOGGLE_DIMENSION_VALUE, dimensionIndex, valueIndex };
+        const state = {
+          ...initialState,
+          dimensions: { [dimensionIndex]: { values: { [valueIndex]: { isToggled: true } } } },
+          compareStale: true,
+        };
+        const expectedState = {
+          ...initialState,
+          dimensions: { [dimensionIndex]: { values: { [valueIndex]: { isToggled: false } } } },
+          compareStale: true,
+        };
+        expect(reducer(state, action)).toEqual(expectedState);
+      });
 
-    it('should handle TOGGLE_DIMENSION_VALUE', () => {
-      const dimensionIndex = 'dimension-index';
-      const valueIndex = 'value-index';
-      const action = { type: TOGGLE_DIMENSION_VALUE, dimensionIndex, valueIndex };
-      const expectedState = {
-        ...initialState,
-        dimensions: { [dimensionIndex]: { values: { [valueIndex]: { isToggled: true } } } },
-        compareStale: true,
-      };
-      expect(reducer(initialState, action)).toEqual(expectedState);
+      it('should match', () => {
+        const dimensionIndex = 'dimension-index';
+        const valueIndex = 'value-index';
+        const action = { type: TOGGLE_DIMENSION_VALUE, dimensionIndex, valueIndex };
+        const expectedState = {
+          ...initialState,
+          dimensions: { [dimensionIndex]: { values: { [valueIndex]: { isToggled: true } } } },
+          compareStale: true,
+        };
+        expect(reducer(initialState, action)).toEqual(expectedState);
+      });
     });
 
     it('should handle TOGGLE_DIMENSION_VALUES', () => {
@@ -136,7 +142,107 @@ describe('web/ducks/data', () => {
       expect(reducer(state, action)).toEqual(expectedState);
     });
 
-    xit('should handle SELECT_DIMENSION_VALUE', () => {});
+    describe('SELECT_DIMENSION_VALUE', () => {
+      it('should match', () => {
+        const valueIndex = 1;
+        const dimensionIndex = 0;
+        const action = { type: SELECT_DIMENSION_VALUE, valueIndex, dimensionIndex };
+        const state = {
+          ...initialState,
+          dimensions: [{ values: [{}, { test: 'test' }] }],
+        };
+        const expectedState = {
+          ...initialState,
+          dimensions: [
+            {
+              values: [
+                {
+                  isToggled: false,
+                  isMapSelected: undefined,
+                  isSelected: false,
+                },
+                {
+                  test: 'test',
+                  isToggled: true,
+                  isMapSelected: undefined,
+                  isSelected: true,
+                },
+              ],
+            },
+          ],
+          countryStale: true,
+          mapStale: undefined,
+        };
+        expect(reducer(state, action)).toEqual(expectedState);
+      });
+
+      it('should match', () => {
+        const valueIndex = 1;
+        const dimensionIndex = 0;
+        const action = { type: SELECT_DIMENSION_VALUE, valueIndex, dimensionIndex };
+        const state = {
+          ...initialState,
+          dimensions: [{ values: [{ isToggled: true }, { test: 'test', isToggled: 'test' }] }],
+        };
+        const expectedState = {
+          ...initialState,
+          dimensions: [
+            {
+              values: [
+                {
+                  isToggled: true,
+                  isMapSelected: undefined,
+                  isSelected: false,
+                },
+                {
+                  test: 'test',
+                  isToggled: 'test',
+                  isMapSelected: undefined,
+                  isSelected: true,
+                },
+              ],
+            },
+          ],
+          countryStale: true,
+          mapStale: undefined,
+        };
+        expect(reducer(state, action)).toEqual(expectedState);
+      });
+
+      it('should match', () => {
+        const valueIndex = 1;
+        const dimensionIndex = 0;
+        const action = { type: SELECT_DIMENSION_VALUE, valueIndex, dimensionIndex };
+        const state = {
+          ...initialState,
+          dimensions: [{ values: [{}, { test: 'test', isRate: true }] }],
+        };
+        const expectedState = {
+          ...initialState,
+          dimensions: [
+            {
+              values: [
+                {
+                  isToggled: false,
+                  isMapSelected: false,
+                  isSelected: false,
+                },
+                {
+                  test: 'test',
+                  isToggled: true,
+                  isMapSelected: true,
+                  isSelected: true,
+                  isRate: true,
+                },
+              ],
+            },
+          ],
+          countryStale: true,
+          mapStale: true,
+        };
+        expect(reducer(state, action)).toEqual(expectedState);
+      });
+    });
 
     it('should handle LOADING_STRUCTURE', () => {
       const action = { type: LOADING_STRUCTURE };
@@ -144,18 +250,20 @@ describe('web/ducks/data', () => {
       expect(reducer(initialState, action)).toEqual(expectedState);
     });
 
-    it('should handle STRUCTURE_LOADED', () => {
-      const dimensions = undefined;
-      const action = { type: STRUCTURE_LOADED, dimensions };
-      const expectedState = { ...initialState, isLoadingStructure: false, dimensions: [] };
-      expect(reducer(initialState, action)).toEqual(expectedState);
-    });
+    describe('STRUCTURE_LOADED', () => {
+      it('should match', () => {
+        const dimensions = undefined;
+        const action = { type: STRUCTURE_LOADED, dimensions };
+        const expectedState = { ...initialState, isLoadingStructure: false, dimensions: [] };
+        expect(reducer(initialState, action)).toEqual(expectedState);
+      });
 
-    it('should handle STRUCTURE_LOADED', () => {
-      const dimensions = 'dimensions';
-      const action = { type: STRUCTURE_LOADED, dimensions };
-      const expectedState = { ...initialState, isLoadingStructure: false, dimensions };
-      expect(reducer(initialState, action)).toEqual(expectedState);
+      it('should match', () => {
+        const dimensions = 'dimensions';
+        const action = { type: STRUCTURE_LOADED, dimensions };
+        const expectedState = { ...initialState, isLoadingStructure: false, dimensions };
+        expect(reducer(initialState, action)).toEqual(expectedState);
+      });
     });
 
     it('should handle LOADING_DATA', () => {
@@ -164,47 +272,51 @@ describe('web/ducks/data', () => {
       expect(reducer(initialState, action)).toEqual(expectedState);
     });
 
-    it('should handle DATA_LOADED', () => {
-      const series = 'series';
-      const staled = undefined;
-      const dataType = 'data-type';
-      const action = { type: DATA_LOADED, series, staled, dataType };
-      const expectedState = {
-        ...initialState,
-        isLoadingData: false,
-        [`${dataType}Series`]: series,
-        [`${dataType}Stale`]: false,
-      };
-      expect(reducer(initialState, action)).toEqual(expectedState);
+    describe('DATA_LOADED', () => {
+      it('should match', () => {
+        const series = 'series';
+        const staled = undefined;
+        const dataType = 'data-type';
+        const action = { type: DATA_LOADED, series, staled, dataType };
+        const expectedState = {
+          ...initialState,
+          isLoadingData: false,
+          [`${dataType}Series`]: series,
+          [`${dataType}Stale`]: false,
+        };
+        expect(reducer(initialState, action)).toEqual(expectedState);
+      });
+
+      it('should match', () => {
+        const series = 'series';
+        const staled = 'staled';
+        const dataType = 'data-type';
+        const action = { type: DATA_LOADED, series, staled, dataType };
+        const expectedState = {
+          ...initialState,
+          isLoadingData: false,
+          [`${dataType}Series`]: series,
+          [`${dataType}Stale`]: staled,
+        };
+        expect(reducer(initialState, action)).toEqual(expectedState);
+      });
     });
 
-    it('should handle DATA_LOADED', () => {
-      const series = 'series';
-      const staled = 'staled';
-      const dataType = 'data-type';
-      const action = { type: DATA_LOADED, series, staled, dataType };
-      const expectedState = {
-        ...initialState,
-        isLoadingData: false,
-        [`${dataType}Series`]: series,
-        [`${dataType}Stale`]: staled,
-      };
-      expect(reducer(initialState, action)).toEqual(expectedState);
-    });
+    describe('TOGGLE_ACTIVE_TYPE', () => {
+      it('should match', () => {
+        const activeType = 'active-type';
+        const action = { type: TOGGLE_ACTIVE_TYPE, activeType };
+        const state = { ...initialState, activeTypes: { [activeType]: true } };
+        const expectedState = { ...initialState, activeTypes: { [activeType]: false } };
+        expect(reducer(state, action)).toEqual(expectedState);
+      });
 
-    it('should handle TOGGLE_ACTIVE_TYPE', () => {
-      const activeType = 'active-type';
-      const action = { type: TOGGLE_ACTIVE_TYPE, activeType };
-      const state = { ...initialState, activeTypes: { [activeType]: true } };
-      const expectedState = { ...initialState, activeTypes: { [activeType]: false } };
-      expect(reducer(state, action)).toEqual(expectedState);
-    });
-
-    it('should handle TOGGLE_ACTIVE_TYPE', () => {
-      const activeType = 'active-type';
-      const action = { type: TOGGLE_ACTIVE_TYPE, activeType };
-      const expectedState = { ...initialState, activeTypes: { [activeType]: true } };
-      expect(reducer(initialState, action)).toEqual(expectedState);
+      it('should match', () => {
+        const activeType = 'active-type';
+        const action = { type: TOGGLE_ACTIVE_TYPE, activeType };
+        const expectedState = { ...initialState, activeTypes: { [activeType]: true } };
+        expect(reducer(initialState, action)).toEqual(expectedState);
+      });
     });
   });
 });
