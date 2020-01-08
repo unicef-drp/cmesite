74ae2fe96a501edb2be0b665aad741dbc3694a34
Nicolas Briemant <nicolas.briemant@gmail.com>
2020-01-08 11:28:53 +0100

    refactor data component to use tabs component

--- src/web/components/Data/index.js
+++ src/web/components/Data/index.js
@@ -1,11 +1,27 @@
-import { compose, branch, renderComponent, withHandlers } from 'recompose';
+import { compose, branch, renderComponent, withHandlers, withProps } from 'recompose';
 import { createStructuredSelector } from 'reselect';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { getActiveTab, getIsLoadingStructure } from '../../selectors/data';
 import { changeActiveTab as changeActiveTabCreator } from '../../ducks/data';
-import Component from './component';
 import Loader from '../Loader';
+import Tabs from '../Tabs';
+import messages from './messages';
+import CountryTab from './country';
+import CompareTab from './compare';
+import MapTab from './map';
+import DownloadTab from './download';
+import countryIcon from '../../../assets/country-tab.png';
+import compareIcon from '../../../assets/compare-tab.png';
+import mapIcon from '../../../assets/map-tab.png';
+import downloadIcon from '../../../assets/download-tab.png';
+
+const tabs = [
+  { key: 'country', icon: countryIcon, component: CountryTab },
+  { key: 'compare', icon: compareIcon, component: CompareTab },
+  { key: 'map', icon: mapIcon, component: MapTab },
+  { key: 'download', icon: downloadIcon, component: DownloadTab },
+];
 
 export default compose(
   withRouter,
@@ -23,4 +39,5 @@ export default compose(
       history.push('/data'); // reset country name is url
     },
   }),
-)(Component);
+  withProps({ tabs, messages }),
+)(Tabs);
